ce9781924b72defb35a1db5f036875fa4b1ba7ee
CAMARA <88945535+camara92@users.noreply.github.com>
2025-07-04 11:27:42 +0200

    maj

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -25,7 +25,7 @@ class HomeController extends AbstractController
             'references'=> $referencesRepository->findAll(),
             'qa'=> $questions, 
             'sevenarticles' => $artilespremiers, 
-            'tests '=> 'Testers via datas controllers ! Daouda '
+          
         ]);
     }
 }
